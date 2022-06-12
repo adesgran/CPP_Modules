f476c7e678210fc2244ee3e092b162e3e086224a
adesgran <adesgran.dev@gmail.com>
2022-06-13 00:31:51 +0200

    Update, need to complete functions

--- Module_0/ex02/Account.cpp
+++ Module_0/ex02/Account.cpp
@@ -6,13 +6,13 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 12:19:08 by adesgran          #+#    #+#             */
-/*   Updated: 2022/06/08 13:10:03 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/13 00:30:17 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Account.hpp>
 #include <iostream>
-#include <time.h>
+#include <ctime>
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
@@ -21,12 +21,26 @@ int	Account::_totalNbWithdrawals = 0;
 
 void	Account::_displayTimestamp(void)
 {
-	time_t		now;
-	struct tm	*tm_now;
+	time_t	now = time(0);
+	tm	*loc_time = localtime(&now);
 
-	now = time(0);
-	tm_now = gmtime(&now);
-	std::cout << tm_now->tm_year << tm_now->tm_mon << tm_now->tm_mday << std::endl;
+	std::cout << "[" << 1900 + loc_time->tm_year;
+	if (loc_time->tm_mon < 10)
+		std::cout << "0";
+	std::cout << loc_time->tm_mon;
+	if (loc_time->tm_mday < 10)
+		std::cout << "0";
+	std::cout << loc_time->tm_mday;
+	std::cout << "_"; 
+	if (loc_time->tm_hour < 10)
+		std::cout << "0";
+	std::cout << loc_time->tm_hour;
+	if (loc_time->tm_min < 10)
+		std::cout << "0";
+	std::cout << loc_time->tm_min;
+	if (loc_time->tm_sec < 10)
+		std::cout << "0";
+	std::cout << loc_time->tm_sec << "] " << std::endl;
 }
 
 Account::Account(int initial_deposit)
@@ -76,3 +90,31 @@ int	Account::getNbWithdrawals(void)
 	return (Account::_totalNbWithdrawals);
 }
 
+void	Account::makeDeposit( int deposit )
+{
+	std::cout << "Deposit" << std::endl;
+	this->_displayTimestamp();
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	std::cout << "Withdraw" << std::endl;
+	(void)withdrawal;
+	return (true);
+}
+
+int		Account::checkAmount( void ) const
+{
+	std::cout << "CheckAmount" << std::endl;
+	return (0);
+}
+
+void	Account::displayStatus( void ) const
+{
+	std::cout << "DisplayStatus" << std::endl;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+}
