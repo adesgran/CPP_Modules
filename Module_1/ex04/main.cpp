70181d6e3edca83d553f11265cb5f6471876474b
Antoine DESGRANGES <adesgran@student.42.fr>
2022-06-14 16:19:22 +0200

    Fix

--- Module_1/ex04/main.cpp
+++ Module_1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 15:18:39 by adesgran          #+#    #+#             */
-/*   Updated: 2022/06/12 16:04:08 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/14 16:17:01 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,19 @@ int	main( int ac, char **av )
 
 	std::string		file_name = av[1];
 	std::ifstream	infile (file_name.c_str());
+	if (!infile || infile.is_open() == false)
+	{
+		std::cout << "\033[0;31mError : " <<  file_name << " is an invalid input file\033[0m" << std::endl;
+		return (1);
+	}
 	file_name.append(".replace");
 	std::ofstream	outfile (file_name.c_str());
+	if (!outfile || outfile.is_open() == false)
+	{
+		infile.close();
+		std::cout << "\033[0;31mError : " <<  file_name << " can be open in write mode\033[0m" << std::endl;
+		return (1);
+	}
 	std::string		line;
 	std::string		s1 = av[2];
 	std::string		s2 = av[3];
