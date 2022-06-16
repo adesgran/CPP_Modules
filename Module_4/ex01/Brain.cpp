ef31b0527b7f1082db142fb5bd9b3b03e169dd79
Antoine DESGRANGES <adesgran@student.42.fr>
2022-06-16 15:43:46 +0200

    Init Brain

--- Module_4/ex01/Brain.cpp
+++ Module_4/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 15:39:54 by adesgran          #+#    #+#             */
-/*   Updated: 2022/06/16 15:39:55 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/16 15:43:26 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 Brain::Brain(void)
 {
-
+	std::cout << "Brain constructor called" << std::endl;
 }
 
 Brain::Brain(const Brain &brain)
 {
-
+	std::cout << "Brain copy constructor called" << std::endl;
+	this->ideas = brain.getIdeas();
 }
 
 Brain::~Brain(void)
 {
-
+	std::cout << "Brain destructor called" << std::endl;
 };
 
 Brain &Brain::operator=(const Brain &brain)
 {
-
+	std::cout << "Brain copy operator called" << std::endl;
+	this->ideas = brain.getIdeas();
+	return (*this);
 }
 
--- Module_4/ex01/Brain.hpp
+++ Module_4/ex01/Brain.hpp
@@ -6,12 +6,15 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 15:39:52 by adesgran          #+#    #+#             */
-/*   Updated: 2022/06/16 15:39:53 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/16 15:43:39 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 # define BRAIN_HPP
+
+# include <iostream>
+# include <string>
 
 class Brain {
 	public:
@@ -19,9 +22,12 @@ class Brain {
 		Brain( const Brain &brain );
 		~Brain( void );
 		Brain &operator=( const Brain &brain );
+		const std::string	[100]getIdeas( void ) const;
+		void	setIdead( std::string ideas[100] );
 
 
 	private:
+		std::string ideas[100];
 
 };
 
