dc2a009c1ede974bbef67609eb09c178d1e01bc9
Antoine DESGRANGES <adesgran@student.42.fr>
2022-07-23 12:44:10 +0200

    Fix

--- Module_8/ex00/easyfind.hpp
+++ Module_8/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 18:42:02 by adesgran          #+#    #+#             */
-/*   Updated: 2022/07/15 19:30:52 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/07/23 12:33:49 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 template< typename T >
 typename T::iterator	easyfind( T container, int x )
 {
-	std::vector<int>::iterator iterator;
+	typename T::iterator iterator;
 
 	iterator = find(container.begin(), container.end(), x);
 	if (iterator == container.end())
